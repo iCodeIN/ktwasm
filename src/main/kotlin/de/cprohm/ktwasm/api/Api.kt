3ea17163752ea36a5df58cd963c72974f8a1fef9
Christopher Prohm <mail@cprohm.de>
2020-02-19 23:19:17 +0100

    Add more classes to the API

--- src/main/kotlin/de/cprohm/ktwasm/api/Api.kt
+++ src/main/kotlin/de/cprohm/ktwasm/api/Api.kt
@@ -2,12 +2,19 @@ package de.cprohm.ktwasm.api
 
 import de.cprohm.ktwasm.base.*
 import de.cprohm.ktwasm.base.Environment
+import de.cprohm.ktwasm.base.F32Value
+import de.cprohm.ktwasm.base.F64Value
 import de.cprohm.ktwasm.base.FunctionRef
 import de.cprohm.ktwasm.base.GlobalRef
+import de.cprohm.ktwasm.base.I32Value
+import de.cprohm.ktwasm.base.I64Value
 import de.cprohm.ktwasm.base.MapEnvironment
 import de.cprohm.ktwasm.base.Memory
 import de.cprohm.ktwasm.base.Namespace
+import de.cprohm.ktwasm.base.Signature
 import de.cprohm.ktwasm.base.Table
+import de.cprohm.ktwasm.base.Type
+import de.cprohm.ktwasm.base.UnitValue
 import de.cprohm.ktwasm.base.WasmValue
 import de.cprohm.ktwasm.binary.parseBinaryModule
 import java.io.File
@@ -23,7 +30,16 @@ typealias Memory = Memory
 typealias FunctionRef = FunctionRef
 typealias GlobalRef = GlobalRef
 typealias Table = Table
+
 typealias WasmValue = WasmValue
+typealias I32Value = I32Value
+typealias I64Value = I64Value
+typealias F32Value = F32Value
+typealias F64Value = F64Value
+typealias UnitValue = UnitValue
+
+typealias Type = Type
+typealias Signature = Signature
 
 fun parseModule(url: URL, env: Environment = EmptyEnvironment()): Namespace =
     parseBinaryModule(url.path, url.readBytes(), env)
